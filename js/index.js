32eaabf43847d1f48dd200992354908f7027c230
Eslam AbuAmer <Eslam.amer@hotmail.com>
2021-01-19 20:19:56 +0200

    update setTime

--- js/index.js
+++ js/index.js
@@ -3,21 +3,25 @@ const insert   = document.querySelector('.insert');
 const button   = document.querySelector('.task button')
 const warning  = document.querySelector('.warning')
 const clearBtn = document.getElementById('clear')
-console.log(insert.value)
+let list
+
 task.addEventListener('submit', function(e){
     e.preventDefault()
     if(insert.value == ""){
         wornMsg()
     }else{
         addTask()
-    }
-})
+        }
+      }) 
 
 function wornMsg(){
     warning.style.display = 'block';
 }
+
+insert.addEventListener('keypress', function() {warning.style.display='none'})
+
 function addTask(){
-    const list        = document.getElementById('list')
+    list              = document.getElementById('list')
     const newTask     = document.createElement('h4')
     const check       = document.createElement('input')
     const element     = document.createElement('div')
@@ -26,25 +30,26 @@ function addTask(){
     element.append(check, newTask)
     element.classList.add('listSty')
     list.appendChild(element)
-    warning.style.display='none'
     insert.value =""
+
     check.addEventListener('click', function(){
         this.hasAttribute('checked')?this.removeAttribute('checked'):this.setAttribute('checked',true)
         if(check.hasAttribute('checked')){
             newTask.style.textDecoration = 'line-through' 
-            setTimeout(() => {
+            set = setTimeout(() => {
              element.remove()
             }, 2000);
         }else{
             newTask.style.textDecoration = 'none'
+            clearTimeout(set);
+            }
         }
-    })
-    clearBtn.onclick = function (){
-    const arr = [...list.children]
-     for(let i=0; i<arr.length; i++){
-         arr[i].remove()
-     }
-        }
-       }
+     )
+ }
   
-   
+clearBtn.onclick = function (){
+    const arr = [...list.children]
+    for(let i=0; i<arr.length; i++){
+    arr[i].remove()
+      }
+}
